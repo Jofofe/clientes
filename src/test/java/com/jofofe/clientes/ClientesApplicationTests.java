c3401b12ea75229ee91d39aa9395d9d383605eb9
João Ferreira <jofelippe@hotmail.com>
2021-09-24 22:03:00 -0300

    Initial commit

---
+++ src/test/java/com/jofofe/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jofofe.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
